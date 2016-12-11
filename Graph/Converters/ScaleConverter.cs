a5ea7eb1f34b7649706550e24e3feb0297c8a810
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2016-12-11 23:35:23 +0100

    Slot relative size

---
+++ Graph/Converters/ScaleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+
+namespace Graph.Converters
+{
+
+	public class ScaleConverter : IValueConverter
+	{
+		//
+		public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+		{
+			return (double)value * double.Parse( parameter.ToString(), CultureInfo.InvariantCulture );
+		}
+
+		//
+		public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+		{
+			return (double)value / double.Parse( parameter.ToString(), CultureInfo.InvariantCulture );
+		}
+	}
+}
