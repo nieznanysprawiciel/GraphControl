cf9fcf8f08e141b788310056cfdccbdef7669a38
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2016-12-11 12:06:53 +0100

    First commit - SlotSocket control introduced

---
+++ Graph/Slot.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+using System.Windows.Documents;
+
+
+
+namespace Graph
+{
+	[TemplatePart( Name = "PART_MainBorder", Type = typeof( Border ) )]
+	[TemplatePart( Name = "PART_TextContent", Type = typeof( ContentControl ) )]
+	[TemplatePart( Name = "PART_Socket", Type = typeof( SlotSocket ) )]
+	public class Slot : Control
+	{
+		Border					MainBorder;
+		SlotSocket				Socket;
+		ContentControl          TextContent;
+
+
+		//// Resources
+		//GradientBrush           m_socketBrush;
+
+		#region DependancyProperties
+
+		public static readonly DependencyProperty SlotColorProperty = DependencyProperty.Register( "SlotColor", typeof( Color ), typeof( Slot ), new PropertyMetadata( Colors.Green ) );
+
+		#endregion
+
+
+
+		static Slot()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata( typeof( Slot ), new FrameworkPropertyMetadata( typeof( Slot ) ) );
+		}
+
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			if( this.Template != null )
+			{
+				Border mainBorder = this.Template.FindName("PART_MainBorder", this) as Border;
+				if( mainBorder != MainBorder )
+				{
+					MainBorder = mainBorder;
+				}
+
+				TextContent = this.Template.FindName( "PART_TextContent", this ) as ContentControl;
+				Socket = this.Template.FindName( "PART_Socket", this) as SlotSocket;
+			}
+		}
+
+		#region Properties
+
+		public Color SlotColor
+		{
+			get
+			{
+				return (Color)this.GetValue( SlotColorProperty );
+			}
+			set
+			{
+				this.SetValue( SlotColorProperty, value );
+			}
+		}
+
+
+		#endregion
+	}
+}
---
+++ Graph/SlotSocket.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Graph
+{
+
+
+	public class SlotSocket : Control
+	{
+
+		#region Dependancy properties
+
+		public static readonly DependencyProperty ColorProperty = DependencyProperty.Register( "Color", typeof( Color ), typeof( SlotSocket ), new PropertyMetadata( Colors.Green ) );
+		public static readonly DependencyProperty SocketBrushProperty = DependencyProperty.Register( "SocketBrush", typeof( RadialGradientBrush ), typeof( SlotSocket ) );
+
+		#endregion
+
+
+		static SlotSocket()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata( typeof( SlotSocket ), new FrameworkPropertyMetadata( typeof( SlotSocket ) ) );
+		}
+
+		public SlotSocket()
+		{
+			SocketBrush = CreateBrush();
+		}
+
+
+		private RadialGradientBrush			CreateBrush	()
+		{
+			var brush = new RadialGradientBrush();
+
+			brush.GradientOrigin = new Point( 0.5, 0.5 );
+			brush.Center = new Point( 0.5, 0.5 );
+
+			GradientStop outerStop = new GradientStop();
+			outerStop.Color = Colors.Blue;
+			outerStop.Offset = 0.824;
+
+			GradientStop innerStop = new GradientStop();
+			outerStop.Color = Color;
+			outerStop.Offset = 1.0;
+
+			brush.GradientStops.Add( outerStop );
+			brush.GradientStops.Add( innerStop );
+
+			return brush;
+		}
+
+
+		#region Properties
+
+		
+		public Color Color
+		{
+			get
+			{
+				return (Color)this.GetValue( ColorProperty );
+			}
+			set
+			{
+				this.SetValue( ColorProperty, value );
+			}
+		}
+
+		public RadialGradientBrush SocketBrush
+		{
+			get
+			{
+				return (RadialGradientBrush)this.GetValue( SocketBrushProperty );
+			}
+
+			set
+			{
+				this.SetValue( SocketBrushProperty, value );
+			}
+		}
+		#endregion
+	}
+}
